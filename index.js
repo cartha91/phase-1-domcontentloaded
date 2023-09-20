cb32f490ac76cf3b034646c74433d596fc695276
steven dulworth <crowscoupetech@proton.me>
2023-09-20 16:28:37 -0500

    updating DOM content

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    let p = document.querySelector('p');
+    if (p.textContent === "JavaScript is so cool. It lets me add text to my page programmatically.") {
+        p.textContent = "This is really cool!";
+    }
+})
